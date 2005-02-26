e32b45a1e0e365bdec59271050b43730e9953438
Scott Ullrich <sullrich@pfsense.org>
2005-02-26 18:20:25 +0000

    Use natearly anchor

--- packages/spamd_rules.php
+++ packages/spamd_rules.php
@@ -27,7 +27,7 @@
 	POSSIBILITY OF SUCH DAMAGE.
 */
 
-$anchor = "natrules";
+$anchor = "natearly";
 $rule = "rdr pass inet proto tcp from <spamd> to any port smtp -> 127.0.0.1 port 8025";
 $label = "spamd";
 add_rule_to_anchor($anchor, $rule, $label);
