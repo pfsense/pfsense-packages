f1609c6ae1f6f9e382bfea1f7b2fa18a99f0449e
Scott Ullrich <sullrich@pfsense.org>
2005-03-31 06:59:12 +0000

    Alter for eval method

--- packages/carp_rules.php
+++ packages/carp_rules.php
@@ -28,7 +28,7 @@
 */
 
 /* return if there are no carp configured items */
-if($config['installedpackages']['carp']['config'] <> "") {
+if($config['installedpackages']['carp']['config'] != "") {
     /* carp records exist, lets process */
     $wan_interface = get_real_wan_interface();
     $i = 0;
@@ -40,7 +40,7 @@ if($config['installedpackages']['carp']['config'] <> "") {
 	$interface = convert_friendly_interface_to_real_interface_name($ifname);
 	add_rule_to_anchor("carp", "pass quick on {$interface} proto carp keep state", $interface . "carp41");
     }
-    if($config['installedpackages']['carp']['config'] <> "")
+    if($config['installedpackages']['carp']['config'] != "")
 	foreach($config['installedpackages']['carp']['config'] as $carp) {
 	    $ip = $carp['ipaddress'];
 	    $int = find_ip_interface($ip);
@@ -48,17 +48,17 @@ if($config['installedpackages']['carp']['config'] <> "") {
 	    add_rule_to_anchor("carp", "pass quick on {$carp_int} proto carp from {$carp_int}:network to 224.0.0.18 keep state \(no-sync\)", $carp_int . "24");
 	    add_rule_to_anchor("carp", "pass out quick on {$carp_int} keep state", $carp_int . "1");
 	    add_rule_to_anchor("carp", "pass quick on {$carp_int} proto carp keep state", $carp_int . "41");
-	    if($int <> false and $int <> $wan_interface) {
+	    if($int != false and $int != $wan_interface) {
 		$ipnet = convert_ip_to_network_format($ip, $carp['netmask']);
 		$rule = "nat on {$int} inet from {$ipnet} to any -> ({$carp_int}) \n";
 		add_rule_to_anchor("natrules", $rule, $ip);
 	    }
 	}
     add_rule_to_anchor("carp", "pass quick on pfsync0 keep state", "pfsync0" . "3");
-    if($config['installedpackages']['carp']['config'] <> "")
+    if($config['installedpackages']['carp']['config'] != "")
 	foreach($config['installedpackages']['carpsettings']['config'] as $carp)
 	    $carp_sync_int = convert_friendly_interface_to_real_interface_name($carp['pfsyncinterface']);
-    if($carp_sync_int <> "") {
+    if($carp_sync_int != "") {
 	add_rule_to_anchor("carp", "pass quick on {$carp_sync_int} proto pfsync", $carp_sync_int . "3");
     }
 }
