8ec029ac813fb9ffb3c0d6d934d5df1e3dbf6cc5
Scott Ullrich <sullrich@pfsense.org>
2006-01-29 04:30:39 +0000

    We're in 06.

--- packages/spamd_db.php
+++ packages/spamd_db.php
@@ -2,7 +2,7 @@
 /* $Id$ */
 /*
 	spamd_db.php
-	Copyright (C) 2004 Scott Ullrich
+	Copyright (C) 2006 Scott Ullrich
 	All rights reserved.
 
 	Redistribution and use in source and binary forms, with or without
