a24dc83e0251ad950c9a1dfc196ddcbd06672f22
Scott Ullrich <sullrich@pfsense.org>
2006-09-29 21:46:59 +0000

    Log when we detect an ip address change so we can verify if this codepath is even being hit.

--- packages/snort/snort_dynamic_ip_reload.php
+++ packages/snort/snort_dynamic_ip_reload.php
@@ -3,11 +3,10 @@ require("/usr/local/pkg/snort.inc");
 
 if($config['interfaces']['wan']['ipaddr'] == "pppoe" or
    $config['interfaces']['wan']['ipaddr'] == "dhcp") {
+		log_error("Snort has detected an IP address change.  Reloading.");
 		stop_service("snort");
 		create_snort_conf();
-		sleep(1);
+		sleep(3);
 		start_service("snort");
-
-
 }
 
