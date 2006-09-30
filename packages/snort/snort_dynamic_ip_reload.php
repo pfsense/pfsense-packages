38add829d9748421cf660fc6c53dae1db531650f
Scott Ullrich <sullrich@pfsense.org>
2006-09-30 23:18:15 +0000

    Note script start and stop

--- packages/snort/snort_dynamic_ip_reload.php
+++ packages/snort/snort_dynamic_ip_reload.php
@@ -1,12 +1,16 @@
 
 require("/usr/local/pkg/snort.inc");
 
+log_error("[SNORT] Snort_dynamic_ip_reload.php is starting.");
+
 if($config['interfaces']['wan']['ipaddr'] == "pppoe" or
    $config['interfaces']['wan']['ipaddr'] == "dhcp") {
-		log_error("Snort has detected an IP address change.  Reloading.");
+		log_error("Snort has detected a dynamic wan address. Reloading configuration.");
 		stop_service("snort");
 		create_snort_conf();
 		sleep(5);
 		start_service("snort");
 }
+
+log_error("[SNORT] Snort_dynamic_ip_reload.php is ending.");
 
