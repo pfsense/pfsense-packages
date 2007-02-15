c6ba3a8dd389e217f93cacd71d1a17f5b636a041
Goffredo Andreone <gandreone@pfsense.org>
2007-02-15 04:47:38 +0000

    Renamed Last Tab Array to Sync vs. Logs

--- packages/tinydns/tinydns_status.php
+++ packages/tinydns/tinydns_status.php
@@ -53,7 +53,7 @@ include("head.inc");
 	$tab_array[] = array(gettext("Add Domains"), false, "/tinydns_filter.php");
 	$tab_array[] = array(gettext("Status"), true, "/tinydns_status.php");
 	$tab_array[] = array(gettext("Logs"), false, "/tinydns_view_logs.php");
-	$tab_array[] = array(gettext("Logs"), false, "/pkg_edit.php?xml=tinydns_sync.xml&id=0");
+	$tab_array[] = array(gettext("Sync"), false, "/pkg_edit.php?xml=tinydns_sync.xml&id=0");
 	display_top_tabs($tab_array);
 ?>
 </table>
