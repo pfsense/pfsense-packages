50e5ba77c4341f084e9238ded88aa129891abfdf
Scott Ullrich <sullrich@pfsense.org>
2007-05-08 23:33:59 +0000

    snort2c does not HUP.  simply kill it and restart.

--- packages/snort/snort_dynamic_ip_reload.php
+++ packages/snort/snort_dynamic_ip_reload.php
@@ -41,7 +41,7 @@ if($config['interfaces']['wan']['ipaddr'] == "pppoe" or
 		create_snort_conf();
 		mwexec("/sbin/pfctl -t snort2c -T flush");
 		exec("killall -HUP snort");
-		exec("killall -HUP snort2c");
+		exec("/usr/bin/killall snort2c; snort2c -w /var/db/whitelist -a /var/log/snort/alert");
 }
 
 
