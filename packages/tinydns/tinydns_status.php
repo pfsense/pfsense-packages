563b47b8ab53e488470c8b541b2f671ce3f36b12
Scott Ullrich <sullrich@pfsense.org>
2008-10-25 02:03:31 +0000

    Rename Add Record to Add/Edit Record

--- packages/tinydns/tinydns_status.php
+++ packages/tinydns/tinydns_status.php
@@ -58,7 +58,7 @@ if(strstr($pfSversion, "1.2"))
 	$tab_array = array();
 	$tab_array[] = array(gettext("Settings"), false, "/pkg_edit.php?xml=tinydns.xml&id=0");
 	$tab_array[] = array(gettext("View Records"), false, "/tinydns_dhcp_filter.php");
-	$tab_array[] = array(gettext("Add Record"), false, "/tinydns_filter.php");
+	$tab_array[] = array(gettext("Add/Edit Record"), false, "/tinydns_filter.php");
 	$tab_array[] = array(gettext("Status"), true, "/tinydns_status.php");
 	$tab_array[] = array(gettext("Logs"), false, "/tinydns_view_logs.php");
 	$tab_array[] = array(gettext("Zone Sync"), false, "/pkg_edit.php?xml=tinydns_sync.xml&id=0");
