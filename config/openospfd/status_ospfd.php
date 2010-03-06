1ab04cff625cfce3f4dda6dc08ac40c2324b453a
Scott Ullrich <sullrich@pfsense.org>
2010-03-06 13:51:32 -0500

    Fixup html.

--- config/openospfd/status_ospfd.php
+++ config/openospfd/status_ospfd.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
     status_ospfd.php
     Copyright (C) 2010 Nick Buraglio; nick@buraglio.com
@@ -35,55 +34,46 @@ include("head.inc");
 
 ?>
 
-<body link="#0000CC" vlink="#0000CC" alink="#0000CC">
-<?php include("fbegin.inc"); ?>
-<p class="pgtitle"><?=$pgtitle?></font></p>
-<?php if ($savemsg) print_info_box($savemsg); ?>
-
-<div id="mainlevel">
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
-<?php
-	$tab_array = array();
-	$tab_array[] = array(gettext("Settings"), false, "/pkg_edit.php?xml=openospfd.xml&id=0");
-	$tab_array[] = array(gettext("Interfaces"), false, "/pkg.php?xml=openospfd_interfaces.xml");
-	$tab_array[] = array(gettext("Status"), true, "/status_ospfd.php");
-	display_top_tabs($tab_array);
-?>
-</table>
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
-   <tr>
-     <td class="tabcont" >
-      <form action="status_ospfd.php" method="post">
-    </form>
-    </td>
-   </tr>
-   <tr>
-    <td class="tabcont" >
-
-<?php
-
-defCmdT("OpenOSPFD Summary","/usr/local/sbin/ospfctl show summary"); 
-defCmdT("OpenOSPFD Neighbors","/usr/local/sbin/ospfctl show neighbor"); 
-defCmdT("OpenOSPFD FIB","/usr/local/sbin/ospfctl show fib");
-defCmdT("OpenOSPFD RIB","/usr/local/sbin/ospfctl show rib"); 
-defCmdT("OpenOSPFD Interfaces","/usr/local/sbin/ospfctl show interfaces"); 
-defCmdT("OpenOSPFD Database","/usr/local/sbin/ospfctl show database"); 
-
-?>
-
-		<div id="cmdspace" style="width:100%">
-		<?php listCmds(); ?>
-		
-		<?php execCmds(); ?>
+<html>
+	<body link="#0000CC" vlink="#0000CC" alink="#0000CC">
+		<?php include("fbegin.inc"); ?>
+		<p class="pgtitle"><?=$pgtitle?></font></p>
+		<?php if ($savemsg) print_info_box($savemsg); ?>
+		<div id="mainlevel">
+			<table width="100%" border="0" cellpadding="0" cellspacing="0">
+			<?php
+				$tab_array = array();
+				$tab_array[] = array(gettext("Settings"), false, "/pkg_edit.php?xml=openospfd.xml&id=0");
+				$tab_array[] = array(gettext("Interfaces"), false, "/pkg.php?xml=openospfd_interfaces.xml");
+				$tab_array[] = array(gettext("Status"), true, "/status_ospfd.php");
+				display_top_tabs($tab_array);
+			?>
+			</table>
+			<table width="100%" border="0" cellpadding="0" cellspacing="0">
+			   <tr>
+			     <td class="tabcont" >
+			      <form action="status_ospfd.php" method="post">
+			    </form>
+			    </td>
+			   </tr>
+			   <tr>
+			    <td class="tabcont" >
+					<?php
+						defCmdT("OpenOSPFD Summary","/usr/local/sbin/ospfctl show summary"); 
+						defCmdT("OpenOSPFD Neighbors","/usr/local/sbin/ospfctl show neighbor"); 
+						defCmdT("OpenOSPFD FIB","/usr/local/sbin/ospfctl show fib");
+						defCmdT("OpenOSPFD RIB","/usr/local/sbin/ospfctl show rib"); 
+						defCmdT("OpenOSPFD Interfaces","/usr/local/sbin/ospfctl show interfaces"); 
+						defCmdT("OpenOSPFD Database","/usr/local/sbin/ospfctl show database"); 
+					?>
+					<div id="cmdspace" style="width:100%">
+						<?php listCmds(); ?>		
+						<?php execCmds(); ?>
+					</div>
+				</td>
+			   </tr>
+			</table>
 		</div>
-
-      </table>
-     </td>
-    </tr>
-</table>
-</div>
-
-<?php include("fend.inc"); ?>
-
-</body>
+		<?php include("fend.inc"); ?>
+	</body>
 </html>
