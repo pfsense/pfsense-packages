a096aa5da24e2a92c04230b0ae56015b63c55b99
Scott Ullrich <sullrich@pfsense.org>
2010-04-27 15:52:20 -0400

    reload config file a tad bit slower

---
+++ config/varnish64/varnish_view_config.php
@@ -0,0 +1,114 @@
+<?php
+/* $Id$ */
+/*
+	varnish_view_config.php
+	part of pfSense (http://www.pfsense.com/)
+
+	Copyright (C) 2010 Scott Ullrich <sullrich@gmail.com>
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice,
+	   this list of conditions and the following disclaimer.
+
+	2. Redistributions in binary form must reproduce the above copyright
+	   notice, this list of conditions and the following disclaimer in the
+	   documentation and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+*/
+
+require("guiconfig.inc");
+
+if($_REQUEST['getactivity']) {
+	$varnishlogs = `cat /var/etc/default.vcl`;
+	echo "Varnish Server configuration as of " . date("D M j G:i:s T Y")  . "\n\n";
+	echo $varnishlogs;
+	exit;
+}
+
+/* Defaults to this page but if no settings are present, redirect to setup page */
+if(!$config['installedpackages']['varnish']['config'][0])
+	Header("Location: /pkg_edit.php?xml=varnish.xml&id=0");
+
+$pfSversion = str_replace("\n", "", file_get_contents("/etc/version"));
+if(strstr($pfSversion, "1.2"))
+	$one_two = true;
+
+$pgtitle = "Varnish: View Logs";
+include("head.inc");
+
+?>
+<body link="#0000CC" vlink="#0000CC" alink="#0000CC">
+<script src="/javascript/scriptaculous/prototype.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		function getlogactivity() {
+			var url = "/varnish_view_config.php";
+			var pars = 'getactivity=yes';
+			var myAjax = new Ajax.Request(
+				url,
+				{
+					method: 'post',
+					parameters: pars,
+					onComplete: activitycallback
+				});
+		}
+		function activitycallback(transport) {
+			$('varnishlogs').innerHTML = '<font face="Courier"><pre>' + transport.responseText  + '</pre></font>';
+			setTimeout('getlogactivity()', 2500);		
+		}
+		setTimeout('getlogactivity()', 10000);	
+	</script>
+<?php include("fbegin.inc"); ?>
+
+<?php if($one_two): ?>
+<p class="pgtitle"><?=$pgtitle?></font></p>
+<?php endif; ?>
+
+<?php if ($savemsg) print_info_box($savemsg); ?>
+
+<div id="mainlevel">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<?php
+	$tab_array = array();
+	$tab_array[] = array(gettext("Backends"), false, "/pkg.php?xml=varnish_backends.xml");
+	$tab_array[] = array(gettext("Settings"), false, "/pkg_edit.php?xml=varnish_settings.xml&id=0");
+	$tab_array[] = array(gettext("Custom VCL"), false, "/pkg_edit.php?xml=varnish_custom_vcl.xml&id=0");
+	$tab_array[] = array(gettext("LB Directors"), false, "/pkg.php?xml=varnish_lb_directors.xml");
+	$tab_array[] = array(gettext("XMLRPC Sync"), false, "/pkg_edit.php?xml=varnish_sync.xml&id=0");
+	$tab_array[] = array(gettext("View Configuration"), true, "/varnish_view-config.php");		
+	display_top_tabs($tab_array);
+?>
+</table>
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+   <tr>
+     <td class="tabcont" >
+      <form action="varnish_view_config.php" method="post">
+		<br>
+			<div id="varnishlogs">
+				<pre>One moment please, loading Varnish logs...</pre>
+			</div>
+     </td>
+    </tr>
+</table>
+<td align="left" valign="top">
+	<form id="filterform" name="filterform" action="varnish_view_config.php" method="post" style="margin-top: 14px;">
+	<p/>
+	<input id="submit" name="clear" type="submit" class="formbtn" value="<?=gettext("Clear log");?>" />
+	</form>
+</td>
+</div>
+<?php include("fend.inc"); ?>
+</body>
+</html>
