b3543bec329f919721a441ed081fb0f9a2da9f32
whizkidzz <github@digitsolutions.nl>
2012-04-06 16:55:20 +0200

    Add "=" records to the failover status page "+" records are "A" records "=" records are "A" records with reverse dns record.

--- config/tinydns/tinydns_status.php
+++ config/tinydns/tinydns_status.php
@@ -105,7 +105,7 @@ foreach($config['installedpackages']['tinydnsdomains']['config'] as $ping) {
 		$status = file_get_contents("/var/db/pingstatus/$monitorip");
 	else
 		$status = "N/A";
-	if(stristr($tinydns_data, "+{$hostname}:{$ipaddress}"))
+	if(stristr($tinydns_data, "+{$hostname}:{$ipaddress}") || stristr($tinydns_data, "={$hostname}:{$ipaddress}") )
 		$inservice = "<FONT COLOR='GREEN'>YES</FONT>";
 	else
 		$inservice = "<FONT COLOR='BLUE'>NO</FONT>";
@@ -157,7 +157,7 @@ foreach($config['installedpackages']['tinydnsdomains']['config'] as $ping) {
 		echo $ipaddress;
 		if($row['loadbalance'])
 			echo " (LB)";
-		if(stristr($tinydns_data, "+{$hostname}:{$row['failoverip']}"))
+		if(stristr($tinydns_data, "+{$hostname}:{$row['failoverip']}") || stristr($tinydns_data, "={$hostname}:{$row['failoverip']}"))
 			$inservice = "<FONT COLOR='GREEN'>YES</FONT>";
 		else
 			$inservice = "<FONT COLOR='BLUE'>NO</FONT>";
