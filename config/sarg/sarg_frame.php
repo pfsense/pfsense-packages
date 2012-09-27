91f7d1ef9ee32bb3a237574be636fb02274328fe
marcelloc <marcellocoutinho@gmail.com>
2012-09-27 12:23:16 -0300

    sarg - fix report access without authentication on sarg_frame.php

--- config/sarg/sarg_frame.php
+++ config/sarg/sarg_frame.php
@@ -27,6 +27,8 @@
 	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 	POSSIBILITY OF SUCH DAMAGE.
 */
+require_once("authgui.inc");
+
 $uname=posix_uname();
 if ($uname['machine']=='amd64')
         ini_set('memory_limit', '250M');
