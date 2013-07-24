73955502c25e6c3e08c3b3f8b19b32409e62d498
Renato Botelho <garga@FreeBSD.org>
2013-07-24 11:54:55 -0300

    Remove empty form with wrong action

--- config/openbgpd/openbgpd_status.php
+++ config/openbgpd/openbgpd_status.php
@@ -165,12 +165,6 @@ function execCmds() {
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
 	<tr>
 		<td class="tabcont" >
-			<form action="tinydns_status.php" method="post">
-			</form>
-		</td>
-	</tr>
-	<tr>
-		<td class="tabcont" >
 
 			<div id="cmdspace" style="width:100%">
 			<?php listCmds(); ?>
