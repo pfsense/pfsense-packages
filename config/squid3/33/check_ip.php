2b744d1d50383d698e198aa5e8f65b95c7399c25
Marcello Coutinho <marcellocoutinho@gmail.com>
2013-09-24 11:27:22 -0300

    squid3-dev - supress php warnings on squid captive portal auth helper

--- config/squid3/33/check_ip.php
+++ config/squid3/33/check_ip.php
@@ -27,7 +27,7 @@
 	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 	POSSIBILITY OF SUCH DAMAGE.
 */
-
+error_reporting(0);
 // stdin loop
 if (! defined(STDIN)) {
         define("STDIN", fopen("php://stdin", "r"));
