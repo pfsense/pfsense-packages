25147efff43f4aa9971e3026a06afe12898d1984
jim-p <jimp@pfsense.org>
2013-10-29 08:52:08 -0400

    Mail Reports: Ensure some variables are actually arrays before using them, avoids an error and some console output if certain portions of a report have never been used. Version bump.

--- config/mailreport/mail_reports_generate.php
+++ config/mailreport/mail_reports_generate.php
@@ -53,9 +53,21 @@ if (!$config['mailreports']['schedule'][$id])
 	exit;
 
 $thisreport = $config['mailreports']['schedule'][$id];
-$cmds = $thisreport['cmd']['row'];
-$logs = $thisreport['log']['row'];
-$graphs = $thisreport['row'];
+
+if (is_array($thisreport['cmd']) && is_array($thisreport['cmd']['row']))
+	$cmds = $thisreport['cmd']['row'];
+else
+	$cmds = array();
+
+if (is_array($thisreport['log']) && is_array($thisreport['log']['row']))
+	$logs = $thisreport['log']['row'];
+else
+	$logs = array();
+
+if (is_array($thisreport['row']))
+	$graphs = $thisreport['row'];
+else
+	$graphs = array();
 
 // If there is nothing to do, bail!
 if ((!is_array($cmds) || !(count($cmds) > 0))
