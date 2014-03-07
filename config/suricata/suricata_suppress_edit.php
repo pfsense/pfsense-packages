6d7dad52b45aae7faab7dd1863b0ef85d365d18e
bmeeks8 <bmeeks8@bellsouth.net>
2014-03-07 16:01:41 -0500

    Remove bogus query string from ALERTS hyperlink tab.

--- config/suricata/suricata_suppress_edit.php
+++ config/suricata/suricata_suppress_edit.php
@@ -142,7 +142,7 @@ if ($savemsg)
 	$tab_array[] = array(gettext("Interfaces"), false, "/suricata/suricata_interfaces.php");
 	$tab_array[] = array(gettext("Global Settings"), false, "/suricata/suricata_global.php");
 	$tab_array[] = array(gettext("Update Rules"), false, "/suricata/suricata_download_updates.php");
-	$tab_array[] = array(gettext("Alerts"), false, "/suricata/suricata_alerts.php?instance={$instanceid}");
+	$tab_array[] = array(gettext("Alerts"), false, "/suricata/suricata_alerts.php");
 	$tab_array[] = array(gettext("Suppress"), true, "/suricata/suricata_suppress.php");
         display_top_tabs($tab_array);
 ?>
