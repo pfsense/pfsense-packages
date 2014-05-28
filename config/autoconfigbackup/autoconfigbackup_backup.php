d6d2afa3e28a3051b39475ec57311b0adeab0f8d
Renato Botelho <garga@FreeBSD.org>
2014-05-28 14:55:16 -0300

    Make sure filesystem is read/write

--- config/autoconfigbackup/autoconfigbackup_backup.php
+++ config/autoconfigbackup/autoconfigbackup_backup.php
@@ -48,7 +48,9 @@ if($_POST) {
 	else 
 		write_config("Backup invoked via Auto Config Backup.");
 	$config = parse_config(true);
+	conf_mount_rw();
 	file_put_contents("/cf/conf/lastpfSbackup.txt", "");
+	conf_mount_ro();
 	upload_config($_REQUEST['reason']);
 	$savemsg = "Backup completed successfully.";
 	$donotshowheader=true;
