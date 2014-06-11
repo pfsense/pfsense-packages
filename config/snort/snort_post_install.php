c0cd1bf2a7473495a6c0edffe9b71e3e2f57bb42
bmeeks8 <bmeeks8@bellsouth.net>
2014-06-11 13:25:24 -0400

    Bump GUI pkg version to 3.0.10 (3.0.11 for 2.2).

--- config/snort/snort_post_install.php
+++ config/snort/snort_post_install.php
@@ -1576,7 +1576,7 @@ if (stristr($config['widgets']['sequence'], "snort_alerts-container") === FALSE)
 	$config['widgets']['sequence'] .= ",{$snort_widget_container}";
 
 /* Update Snort package version in configuration */
-$config['installedpackages']['snortglobal']['snort_config_ver'] = "3.0.9";
+$config['installedpackages']['snortglobal']['snort_config_ver'] = "3.0.10";
 write_config("Snort pkg: post-install configuration saved.");
 
 /* Done with post-install, so clear flag */
