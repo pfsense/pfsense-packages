29d04728a1f94feeefe8a8df07a6343fa554ca9b
bmeeks8 <bmeeks8@bellsouth.net>
2014-07-28 11:42:51 -0400

    Bump Snort GUI package version to v3.1.2 -- bug fixes.

--- config/snort/snort_migrate_config.php
+++ config/snort/snort_migrate_config.php
@@ -459,7 +459,7 @@ unset($r);
 
 // Write out the new configuration to disk if we changed anything
 if ($updated_cfg) {
-	$config['installedpackages']['snortglobal']['snort_config_ver'] = "3.1.1";
+	$config['installedpackages']['snortglobal']['snort_config_ver'] = "3.1.2";
 	log_error("[Snort] Saving configuration settings in new format...");
 	write_config("Snort pkg: migrate existing settings to new format as part of package upgrade.");
 	log_error("[Snort] Settings successfully migrated to new configuration format...");
