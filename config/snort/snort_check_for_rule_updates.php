90ef400842207bfe6dca22f9121d3e3dd3328a21
bmeeks8 <bmeeks8@bellsouth.net>
2014-10-09 17:59:12 -0400

    Flag no config auto-backup since we are only updating a message.

--- config/snort/snort_check_for_rule_updates.php
+++ config/snort/snort_check_for_rule_updates.php
@@ -827,5 +827,5 @@ if ($update_errors)
 else
 	$config['installedpackages']['snortglobal']['last_rule_upd_status'] = gettext("success");
 $config['installedpackages']['snortglobal']['last_rule_upd_time'] = time();
-write_config("Snort pkg: updated status for updated rules package(s) check.");
+write_config("Snort pkg: updated status for updated rules package(s) check.", FALSE);
 ?>
