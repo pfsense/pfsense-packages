bd4191a0c0a81588441d719c2fb465b8e3aca29e
bmeeks8 <bmeeks8@bellsouth.net>
2014-11-03 12:53:38 -0500

    Bump Snort 2.9.6.2 pkg version to 3.1.4 for bug fix.

--- config/snort/snort_post_install.php
+++ config/snort/snort_post_install.php
@@ -263,8 +263,8 @@ if (stristr($config['widgets']['sequence'], "snort_alerts-container") === FALSE)
 	$config['widgets']['sequence'] .= ",{$snort_widget_container}";
 
 /* Update Snort package version in configuration */
-$config['installedpackages']['snortglobal']['snort_config_ver'] = "3.1.3";
-write_config("Snort pkg v3.1.3: post-install configuration saved.");
+$config['installedpackages']['snortglobal']['snort_config_ver'] = "3.1.4";
+write_config("Snort pkg v3.1.4: post-install configuration saved.");
 
 /* Done with post-install, so clear flag */
 unset($g['snort_postinstall']);
