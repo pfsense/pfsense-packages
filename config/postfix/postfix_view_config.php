1b5f6e531fe22e3e0d4477b9f0ef1ffa814ae7de
Renato Botelho <garga@FreeBSD.org>
2014-11-25 10:16:41 -0200

    Make postfix use wrapper binaries on PBIDIR/bin or PBIDIR/sbin instead of try to use regular binaries from PBIDIR/local/(s)bin. etc is also linked from PBIDIR to PBIDIR/local then it won't break anything. It helps ticket #4018

--- config/postfix/postfix_view_config.php
+++ config/postfix/postfix_view_config.php
@@ -29,12 +29,8 @@
 */
 $shortcut_section = "postfix";
 require("guiconfig.inc");
-$pfs_version = substr(trim(file_get_contents("/etc/version")),0,3);
 if (is_dir('/usr/pbi/postfix-' . php_uname("m"))) {
-	if ($pfs_version == 2.2)
-		define('POSTFIX_LOCALBASE', '/usr/pbi/postfix-' . php_uname("m")."/local");
-	else
-		define('POSTFIX_LOCALBASE', '/usr/pbi/postfix-' . php_uname("m"));
+	define('POSTFIX_LOCALBASE', '/usr/pbi/postfix-' . php_uname("m"));
 } else {
 	define('POSTFIX_LOCALBASE','/usr/local');
 }
