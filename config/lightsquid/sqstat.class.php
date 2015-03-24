ad48eb49d6d6506cc40c6b7edae23a3f73ced615
ReinaldoFeitosa <reinaldo@mbcsoftware.com.br>
2015-03-23 21:24:38 -0300

    Update sqstat.class.php
    Fix erro in Squid 3.2 or later

--- config/lightsquid/sqstat.class.php
+++ config/lightsquid/sqstat.class.php
@@ -202,8 +202,12 @@ class squidstat{
 				}
 				if ($connection) {
 					# username field is avaible in Squid 2.6 stable
+					# peer changed to remote in Squid 3.2 or later
+					# me changed to local in Squid 3.2 or later
 					if(substr($v,0,9)  == "username ")  $parsed["con"][$connection]["username"]   = substr($v, 9);
+					if(substr($v,0,7)  == "remote:")    $parsed["con"][$connection]["peer"]       = substr($v, 8);
 					if(substr($v,0,5)  == "peer:")      $parsed["con"][$connection]["peer"]       = substr($v, 6);
+					if(substr($v,0,6)  == "local:")     $parsed["con"][$connection]["me"]         = substr($v, 7);
 					if(substr($v,0,3)  == "me:")        $parsed["con"][$connection]["me"]         = substr($v, 4);
 					if(substr($v,0,4)  == "uri ")       $parsed["con"][$connection]["uri"]        = substr($v, 4);
 					if(substr($v,0,10) == "delay_pool") $parsed["con"][$connection]["delay_pool"] = substr($v, 11);
@@ -423,6 +427,7 @@ class squidstat{
             else
             # i use ip2long() to make ip sorting work correctly
             if ($resolveip) {
+				echo $ip;
                 $hostname = gethostbyaddr($ip);
                 if ($hostname == $ip)
                      $ip = ip2long($ip); # resolve failed. use (ip2long) key
