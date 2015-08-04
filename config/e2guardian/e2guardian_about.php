5cdea65d84747ddad2d2f3c484bce9936c5c05a9
doktornotor <notordoktor@gmail.com>
2015-08-04 12:29:07 +0200

    e2guardian_about.php - code style fixes
    - Fix pfSense website link
    - Fix broken indentation in copyright header
    - XHTML compliance fixes

--- config/e2guardian/e2guardian_about.php
+++ config/e2guardian/e2guardian_about.php
@@ -1,10 +1,12 @@
 <?php
+/* $Id$ */
+/* ========================================================================== */
 /*
 	e2guardian_about.php
 	Copyright (C) 2015 Marcello Coutinho
-	part of pfSense (http://www.pfSense.com)
+	part of pfSense (http://www.pfSense.org/)
 	All rights reserved.
-																			  */
+*/
 /* ========================================================================== */
 /*
 	Redistribution and use in source and binary forms, with or without
@@ -27,7 +29,7 @@
 	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 	POSSIBILITY OF SUCH DAMAGE.
-																			  */
+*/
 /* ========================================================================== */
 
 require_once("guiconfig.inc");
@@ -40,7 +42,7 @@ include("head.inc");
 <?php include("fbegin.inc"); ?>
 
 <?php if ($one_two): ?>
-<p class="pgtitle"><?=$pgtitle?></font></p>
+<p class="pgtitle"><?=$pgtitle?></p>
 <?php endif; ?>
 <?php if ($input_errors) print_input_errors($input_errors); ?>
 <?php if ($savemsg) print_info_box($savemsg); ?>
@@ -48,24 +50,26 @@ include("head.inc");
 
 <div id="mainlevel">
 	<table width="100%" border="0" cellpadding="0" cellspacing="0">
-		<tr><td>
-		<?php
-	$tab_array = array();
-	$tab_array[] = array(gettext("Daemon"), false, "/pkg_edit.php?xml=e2guardian.xml&id=0");
-	$tab_array[] = array(gettext("General"), false, "/pkg_edit.php?xml=e2guardian_config.xml&id=0");
-	$tab_array[] = array(gettext("Limits"), false, "/pkg_edit.php?xml=e2guardian_limits.xml&id=0");
-	$tab_array[] = array(gettext("Blacklist"), false, "/pkg_edit.php?xml=e2guardian_blacklist.xml&id=0");
-	$tab_array[] = array(gettext("ACLs"), false, "/pkg.php?xml=e2guardian_site_acl.xml");
-	$tab_array[] = array(gettext("LDAP"), false, "/pkg.php?xml=e2guardian_ldap.xml&id=0");
-	$tab_array[] = array(gettext("Groups"), false, "/pkg.php?xml=e2guardian_groups.xml&id=0");
-	$tab_array[] = array(gettext("Users"), false, "/pkg_edit.php?xml=e2guardian_users.xml&id=0");
-	$tab_array[] = array(gettext("IPs"), false, "/pkg_edit.php?xml=e2guardian_ips.xml&id=0");
-	$tab_array[] = array(gettext("Report and Log"), false, "/pkg_edit.php?xml=e2guardian_log.xml&id=0");
-	$tab_array[] = array(gettext("Sync"), false, "/pkg_edit.php?xml=e2guardian_sync.xml&id=0");
-	$tab_array[] = array(gettext("Help"), true, "/e2guardian_about.php");
-	display_top_tabs($tab_array);
-?>
-		</td></tr>
+		<tr>
+			<td>
+				<?php
+				$tab_array = array();
+				$tab_array[] = array(gettext("Daemon"), false, "/pkg_edit.php?xml=e2guardian.xml&id=0");
+				$tab_array[] = array(gettext("General"), false, "/pkg_edit.php?xml=e2guardian_config.xml&id=0");
+				$tab_array[] = array(gettext("Limits"), false, "/pkg_edit.php?xml=e2guardian_limits.xml&id=0");
+				$tab_array[] = array(gettext("Blacklist"), false, "/pkg_edit.php?xml=e2guardian_blacklist.xml&id=0");
+				$tab_array[] = array(gettext("ACLs"), false, "/pkg.php?xml=e2guardian_site_acl.xml");
+				$tab_array[] = array(gettext("LDAP"), false, "/pkg.php?xml=e2guardian_ldap.xml&id=0");
+				$tab_array[] = array(gettext("Groups"), false, "/pkg.php?xml=e2guardian_groups.xml&id=0");
+				$tab_array[] = array(gettext("Users"), false, "/pkg_edit.php?xml=e2guardian_users.xml&id=0");
+				$tab_array[] = array(gettext("IPs"), false, "/pkg_edit.php?xml=e2guardian_ips.xml&id=0");
+				$tab_array[] = array(gettext("Report and Log"), false, "/pkg_edit.php?xml=e2guardian_log.xml&id=0");
+				$tab_array[] = array(gettext("Sync"), false, "/pkg_edit.php?xml=e2guardian_sync.xml&id=0");
+				$tab_array[] = array(gettext("Help"), true, "/e2guardian_about.php");
+				display_top_tabs($tab_array);
+				?>
+			</td>
+		</tr>
 		<tr>
 			<td>
 				<div id="mainarea">
@@ -76,25 +80,25 @@ include("head.inc");
 						</tr>
 						<tr>
 							<td width="22%" valign="top" class="vncell"><?=gettext("Blacklists");?></td>
-							<td width="78%" class="vtable"><?=gettext("<a target=_new href='http://www.squidguard.org/blacklists.html'>E2guardian Blacklists</a><br><br>");?>
+							<td width="78%" class="vtable"><?=gettext("<a href='http://www.squidguard.org/blacklists.html'>E2guardian Blacklists</a><br/><br/>");?>
 						</tr>
 						<tr>
 							<td width="22%" valign="top" class="vncell"><?=gettext("Whatis");?></td>
-							<td width="78%" class="vtable"><?=gettext("<a target=_new href='http://e2guardian.org/'>What is E2guardian</a><br><br>");?>
+							<td width="78%" class="vtable"><?=gettext("<a href='http://e2guardian.org/'>What is E2guardian</a><br/><br/>");?>
 						</tr>
 						<tr>
 							<td colspan="2" valign="top" class="listtopic"><?=gettext("About e2guardian pfSense package"); ?></td>
 						</tr>
 						<tr>
-							<td width="22%" valign="top" class="vncell"><?=gettext("Credits ");?></td>
-							<td width="78%" class="vtable"><?=gettext("Package Created by <a target=_new href='http://forum.pfsense.org/index.php?action=profile;u=4710'>Marcello Coutinho</a><br><br>");?></td>
+							<td width="22%" valign="top" class="vncell"><?=gettext("Credits");?></td>
+							<td width="78%" class="vtable"><?=gettext("Package Created by <a href='http://forum.pfsense.org/index.php?action=profile;u=4710'>Marcello Coutinho</a><br/><br/>");?></td>
 						</tr>
 						</table>
 				</div>
 			</td>
 		</tr>
 	</table>
-	<br>
+	<br/>
 	<div id="search_results"></div>
 </div>
 <?php include("fend.inc"); ?>
